1f89c5db19f70fcd5f0f7ae3d5342405151afa26
Saba Lomtadze <slomtadze90@gmail.com>
2022-09-19 08:13:26 +0000

    Update 6 files

--- src/store/Question-slice.js
+++ src/store/Question-slice.js
@@ -3,19 +3,41 @@ import { createSlice } from "@reduxjs/toolkit";
 const businessSlice = createSlice({
   name: "business",
   initialState: {
-    subField: "Kultura",
-    details: { minProfit: "5" },
+    field: "dargi",
+    subField: "qvedargi",
+    details: {
+      minProfit: "",
+      maxProfit: "",
+      exp: "",
+      questions: [
+        { q: "", value: "", id: 1 },
+        { q: "", value: "", id: 2 },
+        { q: "", value: "", id: 3 },
+        { q: "", value: "", id: 4 },
+        { q: "", value: "", id: 5 },
+      ],
+    },
   },
   reducers: {
     addSubField(state, action) {
       state.subField = action.payload;
     },
+    addField(state, action) {
+      state.field = action.payload;
+    },
     addDetails(state, action) {
       state.details = action.payload;
+    },
+    getQuestionValues(state, action) {
+      const existingQuestion = state.details.questions.find(
+        (id) => id == action.payload.id
+      );
+      state.details.questions[existingQuestion].value = action.payload.value;
     },
   },
 });
 
-export const { addSubField, addDetails } = businessSlice.actions;
+export const { addSubField, addDetails, addField, getQuestionValues } =
+  businessSlice.actions;
 
 export default businessSlice.reducer;
