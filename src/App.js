125668765997a977e5252d4d26a697c8ff399c19
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-24 14:20:30 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import styles from "./App.module.css";
 import Inputs from "./inputs/Inputs";
 import Selects from "./Selects/Selects";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <div className={styles.wrapper}>
+    <div className="h-screen w-screen bg-herro-img bg-center bg-cover flex justify-center items-center">
+      {/* <div className="w-3/4 h-3/4 bg-white/90 text-center ">
         <h1>სქორინგი</h1>
         <Selects />
         <Inputs />
-      </div>
+      </div> */}
+
+      <HomePage />
     </div>
   );
 }
