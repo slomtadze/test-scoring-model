68530dfc5e580977b85e16febdc21e3f08bd3ede
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-26 13:49:16 +0000

    Update AdminPageList.jsx and AdminPageListItem.jsx

--- src/components/AdminPageList.jsx
+++ src/components/AdminPageList.jsx
@@ -3,18 +3,13 @@ import AdminPageListItem from "./AdminPageListItem";
 const AdminPageList = () => {
   return (
     <ul className="mb-4 min-h-[120px]">
-      <li>
-        <AdminPageListItem />
-      </li>
-      <li>
-        <AdminPageListItem />
-      </li>
-      <li>
-        <AdminPageListItem />
-      </li>
-      <li>
-        <AdminPageListItem />
-      </li>
+      <AdminPageListItem />
+
+      <AdminPageListItem />
+
+      <AdminPageListItem />
+
+      <AdminPageListItem />
     </ul>
   );
 };
